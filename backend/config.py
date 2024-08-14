9338b41ee25ef627ae16bf1eabfbd4690f2eee7d
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-08-13 23:10:51 -0700

    Started adding workout enums to config

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class AppleHealthPrefix(Enum):
     """Enum of all the Apple Health prefixes"""
     BIOLOGICAL_SEX = "HKBiologicalSex"
@@ -16,7 +15,7 @@ class AppleHealthPrefix(Enum):
 
 
 class HeartRateMotionContext(Enum):
-    """Enum of all the Heart Rate Motion Context options"""
+    """Enum of all the heart rate location options"""
     NOT_SET = 0
     SEDENTARY = 1
     ACTIVE = 2
@@ -24,3 +23,47 @@ class HeartRateMotionContext(Enum):
     @classmethod
     def from_value(cls, value: int) -> str:
         return cls(value).name.replace('_', ' ')
+
+
+
+# https://developer.apple.com/documentation/healthkit/hkworkoutswimminglocationtype
+class SwimmingLocations(Enum):
+    """Enum of all the swimming location options"""
+    UNKNOWN = 0
+    POOL = 1
+    OPEN_WATER = 2
+
+    @classmethod
+    def from_value(cls, value: int) -> str:
+        return cls(value).name.replace('_', ' ')
+
+
+# https://developer.apple.com/documentation/healthkit/hkswimmingstrokestyle 
+class SwimmingStrokeStyles(Enum):
+    """Enum of all the swimming stroke styles"""
+    UNKNOWN = 0
+    MIXED = 1
+    FREESTYLE = 2
+    BACKSTROKE = 3
+    BREASTSTROKE = 4
+    BUTTERFLY = 5
+    KICK_BOARD = 6
+
+    @classmethod
+    def from_value(cls, value: int) -> str:
+        return cls(value).name.replace('_', ' ')
+    
+
+# https://developer.apple.com/documentation/healthkit/hkworkoutsessiontype
+class WorkoutSessionType(Enum):
+    """Enum of all the workout session types"""
+    PRIMARY = 0
+    MIRRORED = 1
+
+    @classmethod
+    def from_value(cls, value: int) -> str:
+        return cls(value).name
+
+# https://developer.apple.com/documentation/healthkit/hkworkoutsessionstate
+
+# https://developer.apple.com/documentation/healthkit/hkworkouteventtype
