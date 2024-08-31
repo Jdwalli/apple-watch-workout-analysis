72633e616f7d86185d6e0954b2157e7ccd4b4470
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-08-31 03:44:33 -0400

    Added WorkoutResponse

--- frontend/src/types/client_types.ts
+++ frontend/src/types/client_types.ts
@@ -10,7 +10,9 @@ export interface ApiResponse<T> {
   data: T;
 }
 
-export interface UploadError {
+
+// Upload Context Interface 
+export interface RequestError {
   errorCode: number;
   errorMessage: string;
 }
@@ -20,9 +22,136 @@ interface UploadContext {
   processingTime: number;
   uploadStartTime: number;
   uploadEndTime: number;
-  errors: UploadError[];
+  errors: RequestError[];
 }
 
+
+// Workout Context Interface 
+export interface WorkoutMetadata {
+  workoutLocationType: string;
+  averageMETs: number;
+  weatherTemperature: number;
+  weatherHumidity: number;
+  timeZone: string;
+  maximumSpeed: number;
+  averageSpeed: number;
+  physicalEffortEstimationType: string;
+  elevationAscended: number;
+  elevationDescended: number;
+  swimmingLocationType: string;
+  swimmingStrokeStyle: string;
+  lapLength: number;
+  swolfScore: number;
+  waterSalinity: number;
+}
+
+export interface WorkoutVitals {
+  heartRate: {
+    chart: Record<string, string[]>;
+    unit: string;
+  };
+}
+
+export interface WorkoutStatistics {
+  heartRate: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+  };
+  activeEnergyBurned: {
+    sum: number;
+    unit: string;
+  };
+  basalEnergyBurned: {
+    sum: number;
+    unit: string;
+  };
+  distanceWalkingRunning: {
+    sum: number;
+    unit: string;
+  };
+  stepCount: {
+    sum: number;
+    unit: string;
+  };
+  runningGroundContactTime: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+    chart: Record<string, string[]>;
+  };
+  runningPower: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+    chart: Record<string, string[]>;
+  };
+  runningVerticalOscillation: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+    chart: Record<string, string[]>;
+  };
+  runningSpeed: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+    chart: Record<string, string[]>;
+  };
+  runningStrideLength: {
+    average: number;
+    minimum: number;
+    maximum: number;
+    unit: string;
+    chart: Record<string, string[]>;
+  };
+  distanceSwimming: {
+    sum: number;
+    unit: string;
+  };
+  swimmingStrokeCount: {
+    sum: number;
+    unit: string;
+  };
+}
+
+export interface WorkoutStats {
+  workoutName: string;
+  workoutDuration: number;
+  workoutDurationUnit: string;
+  workoutTotalDistance: number;
+  workoutTotalDistanceUnit: string;
+  workoutTotalEnergyBurned: number;
+  workoutTotalEnergyBurnedUnit: string;
+  workoutCreationDate: string;
+  workoutStartDate: string;
+  workoutEndDate: string;
+  workoutStatistics: WorkoutStatistics;
+  workoutMetadata: WorkoutMetadata;
+  workoutRoute: string;
+  workoutVitals: WorkoutVitals;
+}
+
+export interface WorkoutContext {
+  statusCode: number;
+  requestedDate: string;
+  workouts: WorkoutStats[];
+  errors: RequestError[];
+}
+
+// API Responses
+
+// /api/upload response
 export interface UploadExportResponse {
   uploadContext: UploadContext;
+}
+
+// /api/workout response
+export interface WorkoutResponse {
+  workoutContext: WorkoutContext;
 }
