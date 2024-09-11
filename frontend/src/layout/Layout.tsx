3181a6e2542f41ab0df5ead2a8e9588f9aa59e34
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-09-11 19:58:01 -0400

    Created base components

---
+++ frontend/src/layout/Layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Layout: React.FC= () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Layout;
---
+++ frontend/src/pages/workouts/components/WorkoutsPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkoutsPage: React.FC= () => {
+  return (
+    <div>
+        Workout Page
+    </div>
+  );
+};
+
+export default WorkoutsPage;
