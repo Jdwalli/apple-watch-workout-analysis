0446b109bed3cda540b51a1002b20062ecc07654
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-09-15 01:19:44 -0400

    Started workout page components

---
+++ frontend/src/pages/workouts/components/WorkoutMetadataOutput.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkoutMetadataOutput: React.FC= () => {
+  return (
+    <div>
+        Workout Metadata Output
+    </div>
+  );
+};
+
+export default WorkoutMetadataOutput;
---
+++ frontend/src/pages/workouts/components/WorkoutPicker.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WorkoutPicker: React.FC= () => {
+  return (
+    <div>
+        Workout Picker
+    </div>
+  );
+};
+
+export default WorkoutPicker;
